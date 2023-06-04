407a1f8511d40d3004f1cc219655d773b6e6fa0d
pyrcl090 <70988666+pyrcl090@users.noreply.github.com>
2023-06-04 13:21:41 -0400

    Add files via upload

---
+++ anaispyrczakinfo/script.js
@@ -0,0 +1,8 @@
+let proj1 = document.querySelector(".proj1");
+let proj2 = document.querySelector(".proj2");
+let proj3 = document.querySelector(".proj3");
+let proj4 = document.querySelector(".proj4");
+let proj5 = document.querySelector(".proj5");
+let proj6 = document.querySelector(".proj6");
+let projet = document.querySelector(".td2");
+
